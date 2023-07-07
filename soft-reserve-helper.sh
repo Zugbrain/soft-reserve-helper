d2a741df9af7e1878116b8aaa233bc56cad56b04
Zugbrain <turtlewow1024@yahoo.com>
2023-07-07 17:10:50 -0600

    script now removes all non-ascii characters (thanks Randy)

--- soft-reserve-helper.sh
+++ soft-reserve-helper.sh
@@ -1,26 +1,27 @@
 #!/bin/sh
 
 # get message bodies from discord input
-grep -v $'\u2014' discord-thread.txt                |\
-grep -v -E '[nN][aA][mM][eE] - [iI][tT][eE][mM]'    |\
-grep '-' > data/messages.txt
+# remove non-ascii: https://stackoverflow.com/questions/3337936/remove-non-ascii-characters-from-csv
+grep -v $'\u2014' discord-thread.txt                            |\
+grep -v -E '[nN][aA][mM][eE] - [iI][tT][eE][mM]'                |\
+grep '-' | perl -pe 's/[^[:ascii:]]//g' > data/messages.txt
 
 # get player names
-cut -d '-' -f 1 data/messages.txt                   |\
+cut -d '-' -f 1 data/messages.txt                               |\
 sed 's/./\u&/' > data/players.txt
 
 # get item names
-cut -d '-' -f 1 --complement data/messages.txt      |\
+cut -d '-' -f 1 --complement data/messages.txt                  |\
 sed -e 's/^[ \t]*//' | sed 's/./\u&/' > data/items.txt
 
 # create SR sheet
-paste -d "#" data/players.txt data/items.txt        |\
-column -t -s '#' | sort -f                          |\
+paste -d "#" data/players.txt data/items.txt                    |\
+column -t -s '#' | sort -f                                      |\
 sed '1s/^/```\n/' > sr.txt
 
 # create SR sheet - items first
-paste -d "#" data/items.txt data/players.txt        |\
-column -t -s '#' | sort -f                          |\
+paste -d "#" data/items.txt data/players.txt                    |\
+column -t -s '#' | sort -f                                      |\
 sed '1s/^/```\n/' > isr.txt
 
 # append ``` to sr.txt & isr.txt so discord interprets it as code
